81b54fa6f042ddad320ae7fa2a3e5da6c9ef18df
Asadbek Beganov <abeganov@c3r7s2.42warsaw.pl>
2025-10-09 18:15:01 +0200

    edit

---
+++ ft_memset.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abeganov <abeganov@student.42warsaw.p      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/09 15:58:31 by abeganov          #+#    #+#             */
+/*   Updated: 2025/10/09 17:12:22 by abeganov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*p;
+
+	p = s;
+	i = 0;
+	while (i < n)
+	{
+		p[i] = (unsigned char) c;
+		i++;
+	}
+	return (s);
+}
