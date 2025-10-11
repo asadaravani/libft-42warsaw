43854c22805af3c16affa185649ecdd0525b67b5
Asadbek Beganov <abeganov@c3r7s2.42warsaw.pl>
2025-10-11 18:42:25 +0200

    edit

--- Makefile
+++ Makefile
@@ -1,1 +1,22 @@
-int	main 
+SRCS		= 	$(wildcard *.c)
+OBJS		= 	$(SRCS:.c=.o)
+CC			=	cc
+RM			=	rm -f
+FLAGS		=	-Wall -Wextra -Werror
+
+NAME		=	libft.a
+
+all:		$(NAME)
+
+$(NAME):	$(OBJS)
+			ar rcs $(NAME) $(OBJS)
+
+clean:
+			$(RM) $(OBJS)
+
+fclean: 	clean
+			$(RM) $(NAME)
+
+re:			fclean $(NAME)
+
+.PHONY:		all clean fclean re
---
+++ ft_memcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abeganov <abeganov@student.42warsaw.p      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/11 16:32:14 by abeganov          #+#    #+#             */
+/*   Updated: 2025/10/11 16:32:25 by abeganov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t				i;
+	unsigned char		*d;
+	const unsigned char	*s;
+
+	if (!dest && !src)
+		return (0);
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	i = 0;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (d);
+}
